78a211a2649c28eab8bc42eed8027be3d366c2bc
Aurélien Rainone <aurelien.rainone@gmail.com>
2018-06-17 15:32:04 +0200

    Initial version, only parse porcelain headers

---
+++ status.go
@@ -0,0 +1,158 @@
+package gitstatus
+
+import (
+	"bufio"
+	"bytes"
+	"fmt"
+	"io"
+	"os/exec"
+	"regexp"
+	"strconv"
+	"unicode/utf8"
+)
+
+// Status represents the status of a Git working tree directory
+type Status struct {
+	NumAdded     int // NumAdded is the number of files added to the index.
+	NumDeleted   int // NumDeleted is the number of files deleted from the index.
+	NumUpdated   int // NumUpdated is the number of files updated in index.
+	NumRenamed   int // NumRenamed is the number of files renamed in index.
+	NumConflicts int // NumConflicts is the number of unmerged files.
+	NumUntracked int // NumUntracked is the number of untracked files.
+
+	CommitSHA1   string // CommitSHA1 is the SHA1 of current commit (or empty in initial state)
+	LocalBranch  string // LocalBranch is the name of the local branch.
+	RemoteBranch string // RemoteBranch is the name of upstream remote branch (tracking).
+	AheadCount   int    // AheadCount indicates by how many commits the local branch is ahead of its upstream branch.
+	BehindCount  int    // BehindCount indicates by how many commits the local branch is behind its upstream branch.
+
+	IsRebased  bool // IsRebased reports wether a rebase is in progress.
+	IsInitial  bool // IsInitial reports wether the working tree is in its initial state (no commit have been performed yet)
+	IsDetached bool // IsDetached reports wether HEAD is not associated to any branch (detached).
+}
+
+// New returns the Status of the Git working tree 'dir'.
+func New(dir string) (*Status, error) {
+	cmd := exec.Command("git", "status", "-uall", "--porcelain=2", "--branch")
+	cmd.Env = append(cmd.Env, "LC_ALL=C")
+	//out, err := cmd.Output()
+	out, err := cmd.StdoutPipe()
+	if err != nil {
+		return nil, fmt.Errorf("can't read command output: %s", err)
+	}
+	defer out.Close()
+
+	st := &Status{}
+	err = st.parsePorcelain(out)
+	if err != nil {
+		return nil, fmt.Errorf("couldn't parse git status: %s", err)
+	}
+
+	return st, nil
+}
+
+func split(data []byte, atEOF bool) (advance int, token []byte, err error) {
+	advance, token, err = bufio.ScanWords(data, atEOF)
+	if err == nil && token != nil && bytes.Equal(token, []byte{'e', 'n', 'd'}) {
+		return 0, []byte{'E', 'N', 'D'}, bufio.ErrFinalToken
+	}
+	return 0, nil, nil
+}
+
+var (
+	// branchOID regex matches:
+	// # branch.oid <commit> | (initial)
+	branchOID = regexp.MustCompile(`^# branch.oid ([a-z0-9]+|\(initial\))$`)
+
+	// branchHEAD regex matches:
+	// branch.head <branch> | (detached)
+	branchHEAD = regexp.MustCompile(`^# branch.head (.*|\(detached\))$`)
+
+	// branchUpstream regex matches:
+	// # branch.upstream <upstream_branch>
+	branchUpstream = regexp.MustCompile(`^# branch.upstream (.+)$`)
+
+	// branchAB regex matches:
+	// # branch.ab +<ahead> -<behind>
+	branchAB = regexp.MustCompile(`^# branch.ab \+([0-9]+)* \-([0-9]+)*$`)
+)
+
+func (st *Status) parseHeader(line string) error {
+	oid := branchOID.FindStringSubmatch(line)
+	if len(oid) == 2 {
+		if oid[1] == "(initial)" {
+			st.IsInitial = true
+		} else {
+			st.CommitSHA1 = oid[1]
+		}
+		return nil
+	}
+
+	head := branchHEAD.FindStringSubmatch(line)
+	if len(head) == 2 {
+		if head[1] == "(detached)" {
+			st.IsDetached = true
+		} else {
+			st.LocalBranch = head[1]
+		}
+		return nil
+	}
+
+	upstream := branchUpstream.FindStringSubmatch(line)
+	if len(upstream) == 2 {
+		st.RemoteBranch = upstream[1]
+		return nil
+	}
+
+	ab := branchAB.FindStringSubmatch(line)
+	if len(ab) == 3 {
+		v, err := strconv.ParseInt(ab[1], 10, 64)
+		if err != nil {
+			return fmt.Errorf("error parsing branch.ab: %v", err)
+		}
+		st.AheadCount = int(v)
+
+		v, err = strconv.ParseInt(ab[2], 10, 64)
+		if err != nil {
+			return fmt.Errorf("error parsing branch.ab: %v", err)
+		}
+		st.BehindCount = int(v)
+		return nil
+	}
+	return nil
+}
+
+// parsePorcelain parses version 2 of Git porcelain status string, ss, and
+// fills the corresponding fields of Status.
+func (st *Status) parsePorcelain(r io.Reader) error {
+	var err error
+	scan := bufio.NewScanner(r)
+	scan.Split(bufio.ScanLines)
+	for scan.Scan() {
+		line := scan.Text()
+		r, _ := utf8.DecodeRuneInString(line)
+		switch r {
+		case '#':
+			err = st.parseHeader(line)
+		case '1':
+			// 'ordinary' changed entries
+		case '2':
+			// renamed or copied entries
+		case 'u':
+			// unmerged entries
+		case '?':
+			// untracked items
+		case '!':
+			// ignored items
+		}
+		if err != nil {
+			return err
+		}
+	}
+
+	if err := scan.Err(); err != nil {
+		return err
+	}
+
+	return nil
+}
---
+++ status_test.go
@@ -0,0 +1,78 @@
+package gitstatus
+
+import (
+	"strings"
+	"testing"
+)
+
+func TestStatus_parsePorcelain(t *testing.T) {
+	tests := []struct {
+		name    string
+		out     string // git status output
+		want    Status
+		wantErr error
+	}{
+		{
+			name: "aligned",
+			out: "" +
+				"# branch.oid 05f8b44d5edc2960eff106e5e780cf83535d0533\n" +
+				"# branch.head master\n" +
+				"# branch.upstream origin/master\n" +
+				"# branch.ab +0 -0\n",
+			want: Status{
+				LocalBranch:  "master",
+				RemoteBranch: "origin/master",
+				CommitSHA1:   "05f8b44d5edc2960eff106e5e780cf83535d0533",
+			},
+		},
+		{
+			name: "diverged",
+			out: "" +
+				"# branch.oid 05f8b44d5edc2960eff106e5e780cf83535d0533\n" +
+				"# branch.head master\n" +
+				"# branch.upstream origin/master\n" +
+				"# branch.ab +1 -3\n",
+			want: Status{
+				LocalBranch:  "master",
+				RemoteBranch: "origin/master",
+				CommitSHA1:   "05f8b44d5edc2960eff106e5e780cf83535d0533",
+				AheadCount:   1,
+				BehindCount:  3,
+			},
+		},
+		{
+			name: "after git init",
+			out: "" +
+				"# branch.oid (initial)\n" +
+				"# branch.head master\n",
+			want: Status{
+				LocalBranch: "master",
+				IsInitial:   true,
+			},
+		},
+		{
+			name: "merge conflicts",
+			out: "" +
+				"# branch.oid ef7516dfd13efbbd8d64a954dfffc82572c1addf\n" +
+				"# branch.head (detached)\n" +
+				"D. N... 100644 000000 000000 aaacdd96fd16226816ba2b7a00b2a6a85363dd8b 0000000000000000000000000000000000000000 LICENSE\n" +
+				"u UU N... 100644 100644 100644 100644 1113757689eecd5df448b25917fed8ef3ae74705 cc3fdf6f829aeb5c794490158e67ffc33cdeae88 c7da7844b226d19f2f02c1072cf0be97075ca2e8 README.md\n",
+			want: Status{
+				CommitSHA1: "ef7516dfd13efbbd8d64a954dfffc82572c1addf",
+				IsDetached: true,
+			},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			got := &Status{}
+			r := strings.NewReader(tt.out)
+			if err := got.parsePorcelain(r); err != tt.wantErr {
+				t.Errorf("parsePorcelain() want error = %+v, got %+v", tt.wantErr, err)
+			}
+			if *got != tt.want {
+				t.Errorf("parsePorcelain() Statuses are not equal\nwant:\t%+v\ngot:\t%+v\n\n", tt.want, *got)
+			}
+		})
+	}
+}
