3771ba9e6791e99ea6ed1aa6cb857a0a94b82430
Aurélien Rainone <aurelien.rainone@gmail.com>
2018-09-21 10:23:57 +0200

    Golint: comment formatting

--- treestate.go
+++ treestate.go
@@ -12,14 +12,30 @@ import (
 type TreeState int
 
 const (
-	Default       TreeState = iota // Default is state set when the working tree is not in any special state.
-	Rebasing                       // Rebasing is the state set when a rebase is in progress, either interactive or manual.
-	AM                             // AM is the state set when a git AM is in progress (mailbox patch).
-	AMRebase                       // AMRebase is the state set when a git AM rebasing is in progress.
-	Merging                        // Merging is the state set when a merge is in progress.
-	CherryPicking                  // CherryPicking is the state when a cherry-pick is in progress.
-	Reverting                      // Reverting is the state when a revert is in progress.
-	Bisecting                      // Bisecting is the state when a bisect is in progress.
+	// Default is state set when the working tree is not in any special state.
+	Default TreeState = iota
+
+	// Rebasing is the state set when a rebase is in progress, either
+	// interactive or manual.
+	Rebasing
+
+	// AM is the state set when a git AM is in progress (mailbox patch).
+	AM
+
+	// AMRebase is the state set when a git AM rebasing is in progress.
+	AMRebase
+
+	// Merging is the state set when a merge is in progress.
+	Merging
+
+	// CherryPicking is the state when a cherry-pick is in progress.
+	CherryPicking
+
+	// Reverting is the state when a revert is in progress.
+	Reverting
+
+	// Bisecting is the state when a bisect is in progress.
+	Bisecting
 )
 
 // MarshalJSON returns the JSON encoding of the tree state.
