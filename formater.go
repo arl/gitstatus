b1e5852d69ade9c463cf8a239e3728474ce6007f
Aurélien Rainone <aurelien.rainone@gmail.com>
2019-11-05 07:53:17 +0100

    format: remove format related types

--- formater.go
+++
@@ -1,7 +0,0 @@
-package gitstatus
-
-// Formater is the interface implemented by objects having a Format method.
-type Formater interface {
-	// Format returns the string representation of a given Status.
-	Format(*Status) (string, error)
-}
